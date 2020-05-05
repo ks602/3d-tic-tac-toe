89b6194d80e95a7085f2b9e577071e2896543bcd
Wong Andy <andyjack0602@yahoo.com.hk>
2020-05-06 00:07:01 +0800

    implemented game loop

--- proj.py
+++ proj.py
@@ -1,20 +1,58 @@
-def displayBoard(grid):
-    for layer in grid:
-        for row in layer:
-            for cell in row:
-                print(cell, "/", end="")
-            print()
-            print("—— —— ——")
-    print()
+class Board():
+    def __init__(self):
+        n = 3
+        self.board = [[[" " for k in range(n)]
+                       for j in range(n)] for i in range(n)]
+
+    def displayBoard(self):
+        board = ""
+        for i in range(3):
+            board += "    --- --- ---\n"
+            for j in range(3):
+                board += (3-j)*" "
+                board += "/"
+                for k in range(3):
+                    board += str(self.board[i][j][k])
+                    board += " / "
+                board += "\n"
+                board += (3-j)*" "
+                board += "--- --- ---\n"
+            board += "\n"
+        print(board)
+
+    def checkEndGame(self, player):
+        return None
+
+    def placeMove(self, player, position):
+        self.board[position[0]][position[1]//3][position[1] % 3] = player
+        if (player != human):
+            print("Computer placed moved at", position[0], position[1])
 
 
-def initBoard():
-    n = 3
-    grid = [[[0 for k in range(n)] for j in range(n)] for i in range(n)]
-    return grid
+def getInputUser():
+    layer = int(input("State the layer(0, 1, 2) you want to place your move: "))
+    index = int(input("State the position(0-9) you want to place your move: "))
+    return [layer, index]
+
+
+def getInputAI(b, player):
+    return [1, 1]
 
 
 if __name__ == "__main__":
-    grid = initBoard()
-    print(grid)
-    displayBoard(grid)
+    b = Board()
+    human = "X"
+    b.displayBoard()
+    player = "X"
+    while True:
+        move = getInputUser() if (player == human) else getInputAI(b, player)
+        b.placeMove(player, move)
+        b.displayBoard()
+        endGame = b.checkEndGame(player)
+        if (endGame != None):
+            if (endGame):
+                print(player, "won the game!")
+            else:
+                print("Tie")
+            break
+        player = "X" if (player == "O") else "O"
