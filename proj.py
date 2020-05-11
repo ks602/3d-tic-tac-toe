f6a7b103b2e2e14ae4dff9323612e0cba8b3b298
ks602 <andyjack0602@yahoo.com.hk>
2020-05-11 17:24:06 +0800

    added winner-checking and tie-checking

--- proj.py
+++ proj.py
@@ -1,8 +1,20 @@
 class Board():
     def __init__(self):
-        n = 3
-        self.board = [[[" " for k in range(n)]
-                       for j in range(n)] for i in range(n)]
+        self.n = 3
+        self.board = [[[" " for k in range(self.n)]
+                       for j in range(self.n)] for i in range(self.n)]
+        # self.board = [
+        #     [[" ", " ", " "],
+        #      [" ", " ", " "],
+        #      ["X", " ", " "]],
+
+        #     [[" ", " ", " "],
+        #      ["X", " ", " "],
+        #      [" ", " ", " "]],
+
+        #     [["X", " ", " "],
+        #      [" ", " ", " "],
+        #      [" ", " ", " "]]]
 
     def displayBoard(self):
         board = ""
@@ -20,13 +32,132 @@ class Board():
             board += "\n"
         print(board)
 
-    def checkEndGame(self, player):
-        return None
+    def checkWinGame(self, player, move):
+        layer = move[0]
+        x = move[1]//3
+        y = move[1] % 3
 
-    def placeMove(self, player, position):
-        self.board[position[0]][position[1]//3][position[1] % 3] = player
+        # check same layer
+        # check row
+        for i in range(self.n):
+            if (self.board[layer][x][i] != player):
+                break
+            if (i == self.n-1):
+                return True
+
+        # check column
+        for i in range(self.n):
+            if (self.board[layer][i][y] != player):
+                break
+            if (i == self.n-1):
+                return True
+
+        # check diagonal
+        if (x == y):
+            for i in range(self.n):
+                if (self.board[layer][i][i] != player):
+                    break
+                if (i == self.n-1):
+                    return True
+
+        # check anti-diagonal:
+        if (x+y == self.n-1):
+            for i in range(self.n):
+                if (self.board[layer][i][self.n-1-i] != player):
+                    break
+                if (i == self.n-1):
+                    return True
+
+        # check across layer
+        # check vertical
+        for i in range(self.n):
+            if (self.board[i][x][y] != player):
+                break
+            if (i == self.n-1):
+                return True
+
+        # check diagonal in y-axis
+        if (layer == x):
+            for i in range(self.n):
+                if (self.board[i][i][y] != player):
+                    break
+                if (i == self.n-1):
+                    return True
+
+        # check anti-diagonal in y-axis
+        if (layer+x == self.n-1):
+            for i in range(self.n):
+                if (self.board[i][self.n-1-i][y] != player):
+                    break
+                if (i == self.n-1):
+                    return True
+
+        # check diagonal in x-axis
+        if (layer == y):
+            for i in range(self.n):
+                if (self.board[i][x][i] != player):
+                    break
+                if (i == self.n-1):
+                    return True
+
+        # check anti-diagonal in x-axis
+        if (layer+y == self.n-1):
+            for i in range(self.n):
+                if (self.board[i][x][self.n-1-i] != player):
+                    break
+                if (i == self.n-1):
+                    return True
+
+        # check diagonal from (0,0,0) to (2,2,2)
+        if (layer == x == y):
+            for i in range(self.n):
+                if (self.board[i][i][i] != player):
+                    break
+                if (i == self.n-1):
+                    return True
+
+        # check diagonal from (0,0,2) to (2,2,0)
+        for i in range(self.n):
+            if (self.board[i][i][self.n-1-i] != player):
+                break
+            if (i == self.n-1):
+                return True
+
+        # check diagonal from (0,2,0) to (2,0,2)
+        for i in range(self.n):
+            if (self.board[i][self.n-1-i][i] != player):
+                break
+            if (i == self.n-1):
+                return True
+
+        # check diagonal from (0,2,2) to (2,0,0)
+        if (x == y):
+            for i in range(self.n):
+                if (self.board[i][self.n-1-i][self.n-1-i] != player):
+                    break
+                if (i == self.n-1):
+                    return True
+        return False
+
+    def checkEndGame(self):
+        for layer in self.board:
+            for row in layer:
+                for cell in row:
+                    if (cell == " "):
+                        return False
+        return True
+
+    def placeMove(self, player, move):
+        if (move[0] > 2 or move[0] < 0):
+            return False
+        if (move[1] > 8 or move[0] < 0):
+            return False
+        if (self.board[move[0]][move[1]//3][move[1] % 3] != " "):
+            return False
+        self.board[move[0]][move[1]//3][move[1] % 3] = player
         if (player != human):
-            print("Computer placed moved at", position[0], position[1])
+            print("Computer placed moved at", move[0], move[1])
+        return True
 
 
 def getInputUser():
@@ -45,14 +176,18 @@ if __name__ == "__main__":
     b.displayBoard()
     player = "X"
     while True:
-        move = getInputUser() if (player == human) else getInputAI(b, player)
-        b.placeMove(player, move)
+        while True:
+            move = getInputUser() if (player == human) else getInputUser()
+            if (b.placeMove(player, move)):
+                break
         b.displayBoard()
-        endGame = b.checkEndGame(player)
-        if (endGame != None):
-            if (endGame):
-                print(player, "won the game!")
-            else:
-                print("Tie")
+        won = b.checkWinGame(player, move)
+        if (won):
+            print(player, "won the game!")
+            break
+
+        endGame = b.checkEndGame()
+        if (endGame):
+            print("Tie!")
             break
         player = "X" if (player == "O") else "O"
